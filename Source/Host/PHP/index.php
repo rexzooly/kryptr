b6602c44f6f9f945c7d60a97e78306d44e1a5923
Rexzooly Kai Black <rexzooly@gmail.com>
2013-08-17 01:09:46 +0100

    PHP API Placeholder file

---
+++ Source/Host/PHP/index.php
@@ -0,0 +1,27 @@
+<?php 
+###################################################################################################
+#                                                                                                 #
+#                                                                                                 #
+#                                  Kryptr Direct Messaging System                                 #
+#                                                                                                 #
+#                                                                                                 #
+###################################################################################################
+#Version: 0.0.0.0
+#Author: Rexzooly Kai Black
+#Advisor: Shadow(Shadiku)
+
+//Host Name, many hosts by default is set as localhost
+define('K_Host', 'localhost');
+//Database Name.
+define('K_Name', '');
+//Database Username.
+define('K_User', '');
+//Database Password.
+define('K_Auth', '');
+
+
+//Check is the apicall was made
+if(isset($_REQUEST['apicall'])){
+
+}
+?>
