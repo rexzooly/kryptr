8d6b2471cfd4fbba3117b8e6c3a81006b9a9066f
Rexzooly Kai Black <rexzooly@gmail.com>
2013-08-20 12:22:12 +0100

    Fixed the K_Return option
    Changed K_Return from a define to a vrb, fixed a broken call

--- Source/Host/PHP/index.php
+++ Source/Host/PHP/index.php
@@ -22,14 +22,19 @@ define('K_Auth', '');
 //API mode DB for Database mode, FF for Flat File Mode.
 define('K_Mode', 'DB');
 //API Return Mode JSON or HTML can be over ridden with &s=json, &s=html or $s=txt, if you set a invalied s it will default to html
-define('K_Return', 'html');
+$K_Return = 'html';
 //Here we define the apicall pool
-$CallPool = array('call' => array('ip', 'username', 'login', 'myip'), 'mode'=>array('json','html','txt'));
+$CallPool = array(
+	//The API Call List
+	'call' => array('ip', 'username', 'login', 'myip'),
+	//The API Style List
+	'mode'=>array('json', 'j', 'html', 'h', 'txt', 'text', 't')
+);
 
 //Chec Style/Mode
-if(!isset($_REQUEST['s'])){
+if(isset($_REQUEST['s'])){
 	if(in_array($_REQUEST['s'], $CallPool['mode'])){
-		define('K_Return', $_REQUEST['s']);
+		$K_Return = $_REQUEST['s'];
 	}
 }
 
@@ -43,20 +48,20 @@ if(!isset($_REQUEST['apicall'])){
 	}else{
 		//This Option is just for tested and for another project I am working on, I will leave it in for testing.
 		if($_REQUEST['apicall'] == 'myip'){
-			echo ReturnPrint(K_Return, (!empty($_SERVER['REMOTE_ADDR'])) ? $_SERVER['REMOTE_ADDR'] : getenv('REMOTE_ADDR'));
+			echo ReturnPrint($K_Return, (!empty($_SERVER['REMOTE_ADDR'])) ? $_SERVER['REMOTE_ADDR'] : getenv('REMOTE_ADDR'));
 		}
 	}
 }
 //Placeholder functions for later use.
 function ReturnPrint($Mode='html', $e_msg='', $pass='0'){
-	if($Mode == 'html'){
-		return '';
+	if($Mode == 'html'|| $Mode == 'h'){
+		return 'html';
 	}
-	if($Mode == 'json'){
+	if($Mode == 'json' || $Mode == 'j'){
 		return json_encode(array('pass'=>$pass,'return'=>$e_msg));
 	}
-	if($Mode == 'text'){
-		return '';
+	if($Mode == 'txt' || $Mode == 'text' || $Mode == 't'){
+		return 'text';
 	}
 }
 ?>
