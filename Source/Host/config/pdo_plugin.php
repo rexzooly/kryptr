97f7b10db898ca820c0a35364f9d898e98f1e9ee
Rexzooly Kai Black <rexzooly@gmail.com>
2013-08-20 21:40:27 +0100

    Created the PDO Check User
    This is the MYSQL PDO Driver connection at this time it as the user
    check function.

--- Source/Host/config/pdo_plugin.php
+++ Source/Host/config/pdo_plugin.php
@@ -1,3 +1,30 @@
 <?php
-//Placeholder
+###################################################################################################
+#                                                                                                 #
+#                                                                                                 #
+#                                     Kryptr PDO Plugin                                           #
+#                                                                                                 #
+#                                                                                                 #
+###################################################################################################
+function kPDOon(){
+	$Connection = new PDO("mysql:host=" . K_Host . ";dbname=" . K_Name . ";", K_User, K_Auth);
+	$Connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $Connection;
+}
+function kPDOoff(){
+	$Connection = null;
+	return $Connection;
+}
+function usercheck($k_name){
+	$Connection = kPDOon();
+	$sql = $Connection->prepare('SELECT * FROM kryptr WHERE user=:nid');
+	$sql->execute(array('nid' => $k_name));
+	$result = $sql->fetch();
+	if(count($result) == 0 || $result == ''){
+		return false;
+	}else{
+		return true;
+	}
+	$Connection = kPDOoff();
+}
 ?>
